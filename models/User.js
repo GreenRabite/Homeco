bb223efe784758f6022faad146e9c62a5fbe0c5a
luuufan <angelia.fan@gmail.com>
2018-02-18 12:00:57 -0800

    add userSchema.pre to salt/hash password, pending on userSchema

--- models/User.js
+++ models/User.js
@@ -7,6 +7,26 @@ const userSchema = new Schema({
   googleName: String
 });
 
+userSchema.pre('svae', (next)=>{
+  const user = this;
+  if (this.isModified('password') || this.isNew) {
+    bcrypt.getSalt(10, (err, salt)=>{
+      if (err) {
+        return next(err);
+      }
+      bcrypt.hash(user.password, salt, (err, hash)=>{
+        if (err) {
+          return next(err);
+        }
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    return next();
+  }
+});
+
 userSchema.methods.comparePwd = function (pwd, callback){
   bcrypt.compare(pwd, this.password, (err, isMatch)=>{
     if (err) {
