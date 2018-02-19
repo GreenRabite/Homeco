7f4b5effec1b8060d846c39f580d80978f36ca13
Aluo <green.rabite@gmail.com>
2018-02-19 10:20:22 -0800

    User Auth Pred

--- models/User.js
+++ models/User.js
@@ -1,11 +1,43 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const bcrpt = require('bcrypt');
 
 const userSchema = new Schema({
+  _id: mongoose.Schema.Types.ObjectId,
   googleID: String,
   googleName: String,
-  userName: String,
-  passwordDigest: String
+  userName: {type: String, required: true, unique: true },
+  passwordDigest: {type: String, required: true},
+  email: {type: String, required: true, unique: true}
 });
+
+// userSchema.pre('save', function (next) {
+//     let user = this;
+//     if (this.isModified('password') || this.isNew) {
+//         bcrypt.genSalt(10, function (err, salt) {
+//             if (err) {
+//                 return next(err);
+//             }
+//             bcrypt.hash(user.password, salt, function (err, hash) {
+//                 if (err) {
+//                     return next(err);
+//                 }
+//                 user.password = hash;
+//                 next();
+//             });
+//         });
+//     } else {
+//         return next();
+//     }
+// });
+//
+// userSchema.methods.comparePassword = function (passw, cb) {
+//     bcrypt.compare(passw, this.password, function (err, isMatch) {
+//         if (err) {
+//             return cb(err);
+//         }
+//         cb(null, isMatch);
+//     });
+// };
 
 mongoose.model('users',userSchema);
