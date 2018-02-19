adba29677467ec1315435e59fb2647295d1ab4a9
luuufan <angelia.fan@gmail.com>
2018-02-18 16:01:28 -0800

    comment out sessionController, not sure we need controller or not now for user/session

--- controllers/SessionController.js
+++ controllers/SessionController.js
@@ -1,22 +1,20 @@
 const User = require('../model/User');
 
-module.exports = {
-  const create = (params, callback) => {
-    User.findOne({username: username}, (err, user)=>{
-      if (err) {
-        return callback(err, null)
-      }
-      callback(null, user)
-    })
-  };
 
-  const destroy = (params, callback) => {
-    //Session.findBySessionTokenAndRemove(sessionToken, (err)=>{
-      // if (err) {
-      //   return callback(err, null)
-      // }
-      // callback(null, null)
-  // })
-  };
+module.exports = {
+
+  // not sure we need session controller now, cover by passport LocalStrategry
+  // const create = (params, callback) => {
+  //   User.findOne({username: username}, (err, user)=>{
+  //     if (err) {
+  //       return callback(err, null)
+  //     }
+  //     callback(null, user)
+  //   })
+  // };
+
+  // const destroy = (params, callback) => {
+  //
+  // };
 
 }
