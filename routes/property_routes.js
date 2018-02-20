020ba4a2662e30bed867b8f6253b8c476377f821
Aluo <green.rabite@gmail.com>
2018-02-20 11:18:29 -0800

    Remove property routes from backend

--- routes/property_routes.js
+++ routes/property_routes.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const Property = mongoose.model('properties');
 
-module.exports = app => {
-  app.post("/api/properties", (req,res) => {
-    const { address, _user } = req.body;
-    const property = new Property({
-      address,
-      _user
-    });
-  });
-};
+// module.exports = app => {
+//   app.post("/api/properties", (req,res) => {
+//     const { address, _user } = req.body;
+//     const property = new Property({
+//       address,
+//       _user
+//     });
+//   });
+// };
