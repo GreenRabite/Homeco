e8792c37d926933b0c30d9e822ffd352be82f09d
learnor <learnor@gmail.com>
2018-02-19 20:02:29 -0800

    add login

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        retrun (
+          <li><a href="/auth/google">Login with Google</a></li>
+        );
+      default:
+        return <li><a>Logout</a></li>
+    }
+  }
   render() {
     return (
       <nav>
@@ -19,4 +32,7 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+export default connect()(Header);
