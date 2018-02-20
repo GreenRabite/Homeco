15c5bb03ec03235c188195730e5692336fbc0c6d
learnor <learnor@gmail.com>
2018-02-19 19:25:22 -0800

    add materialize css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,7 +9,7 @@ const Landing = () => <h2>Landing</h2>
 
 const App = () => {
   return (
-    <div>
+    <div className="container">
       <BrowserRouter>
         <div>
           <Header />
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,1 +1,22 @@
 import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a className="left brand-logo">
+          Homeco
+          </a>
+          <ul className="right">
+            <li>
+              <a> Login with Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Header;
