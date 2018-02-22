bddc67b497c48e887d8f177b16245d1064bd7be8
learnor <learnor@gmail.com>
2018-02-21 23:21:20 -0800

    correct component

--- frontend/components/profile/profile_container.js
+++ frontend/components/profile/profile_container.js
@@ -6,12 +6,11 @@ import { receiveSchedule, receiveSchedules } from '../../actions/schedule_action
 import Profile from './profile';
 
 const mapStateToPros = (state) => ({
-  schedule: state.entities.schedules
+
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  receiveSchedule: (schedule) => dispatch(receiveSchedule(schedule)),
-  receiveSchedules: (schedules) => dispatch(receiveSchedules(schedules))
+
 });
 
 export default connect(
--- frontend/components/schedule/schedule_container.js
+++ frontend/components/schedule/schedule_container.js
@@ -1,17 +1,16 @@
 import { connect } from 'react-redux';
 import { receiveSchedule } from '../../actions/schedule_actions'
 import ScheduleIndex from './schedule_index';
+import { receiveSchedule, receiveSchedules } from '../../actions/schedule_actions'
 
+const mapStateToPros = (state) => ({
+  schedule: state.entities.schedules
+});
 
-const mapStateToPros = (state) => {
-
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return({
-    receiveSchedule: () => dispatch()
-  });
-};
+const mapDispatchToProps = (dispatch) => ({
+  receiveSchedule: (schedule) => dispatch(receiveSchedule(schedule)),
+  receiveSchedules: (schedules) => dispatch(receiveSchedules(schedules))
+});
 
 
 export defult connect(
