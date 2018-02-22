b8d2c405bf1a17e935481bbbdc544ca18303ed4f
learnor <learnor@gmail.com>
2018-02-22 08:48:13 -0800

    schedule index component

--- frontend/components/schedule/schedule_index.jsx
+++ frontend/components/schedule/schedule_index.jsx
@@ -4,10 +4,12 @@ import ScheduleIndexItem from './schedule_index_item';
 class ScheduleIndex extends Component {
 
   render(){
+    const {schedules} = this.props;
     return(
       <div>
+        <h2> My Schedule </h2>
         <ul>
-          <ScheduleIndexItem workDate={this.props.workDate} />
+          {schedules.map(schedule => <ScheduleIndexItem workDate={schedule.workDate} key={schedule.id}/> )}
         </ul>
       </div>
     );
