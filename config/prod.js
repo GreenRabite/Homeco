72e1bf8d4d26c9e2c21b69998ef71c1fc5b95b0f
learnor <learnor@gmail.com>
2018-02-25 14:43:24 -0800

    add production key config

--- config/prod.js
+++ config/prod.js
@@ -4,5 +4,8 @@ module.exports = {
   googleClientID : process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+  ZWSID: process.env.ZWSID,
+  CLOUD_NAME: process.env.CLOUD_NAME,
+  UPLOAD_PRESET: process.env.UPLOAD_PRESET
 };
