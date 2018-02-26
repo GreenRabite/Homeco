a7a0840a0b5c5328dfa2f5292bf7c048437a87db
Aluo <green.rabite@gmail.com>
2018-02-25 18:27:33 -0800

    Contractors tasks now show what type of work it is

--- frontend/components/contractor/contractor_form.jsx
+++ frontend/components/contractor/contractor_form.jsx
@@ -59,14 +59,21 @@ class ContractorForm extends React.Component {
       const street = this.props.schedule._package._property.street;
       const city = this.props.schedule._package._property.city;
       const state = this.props.schedule._package._property.state;
-
+      let serviceCategoryIcon = {
+        "contractor": "http://res.cloudinary.com/greenrabite/image/upload/v1519610083/02_contractor.png",
+        "landscape": "http://res.cloudinary.com/greenrabite/image/upload/v1519610082/01_lawn_care.svg",
+        "home clean": "http://res.cloudinary.com/greenrabite/image/upload/v1519610083/03_home_clean.png",
+        "pest": "http://res.cloudinary.com/greenrabite/image/upload/v1519610083/04_no_pest.png"
+      };
       return(
         <div className="contractor-form-container">
-          <img src="https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?w=320&h=200&auto=compress&cs=tinysrgb"></img>
+          <div>
+            <img className="icon-container" src={`${serviceCategoryIcon[this.props.schedule._service.category]}`}></img>
+          </div>
           <div>{street}</div>
           <div>{`${city}, ${state}`}</div>
           <form >
-            <textarea cols="40" rows="2" charswidth="23" onChange={this.updateInput("description")} placeholder="Description of Task" value={this.state.description}></textarea><br/> <br/>
+            <textarea cols="40" rows="4" charswidth="23" onChange={this.updateInput("description")} placeholder="Description of Services Rendered" value={this.state.description}></textarea><br/>
             <button className="button" onClick={this.uploadImage}>Upload Photo</button> <br/>
             <button className="button" onClick={this.handleSubmit}>Submit</button> <br/>
             <br/>
